90d0989650e987ce25dbb8a5de9a98f357c72252
anushnap <anushnap@uw.edu>
2021-02-17 21:14:12 -0800

    added parent-only ingredient column and changed logic to split ingredients

--- data_prep.py
+++ data_prep.py
@@ -3,22 +3,34 @@ import pandas as pd
 import numpy as np
 import re
 
+# Adds ingredients_as_list column to products dataframe containing list of comma-separated 
+# discrete ingredients
 def split_ingredients(products):
-    ingr_dict = {}
-    products['ingredients_as_list'] = np.nan # Create new col with list of ingredients
+    products['ingredients_as_list'] = products['ingredients'].apply(lambda x: process_ingredients_to_list(x))
+    products['parent_ingredients_as_list'] = products['ingredients_as_list'].apply(lambda x: get_parent(x))
 
-    for i in range(len(products)):
-        ingr_i = products['ingredients'][i].lower()
-        ingr_split = re.split(r'[.,]\s*(?![^()]*\))', ingr_i) # split on commas, ignore commas inside paren
-        products['ingredients_as_list'][i] = ingr_split
+    # for i in range(len(products)):
+    #     ingr_i = products['ingredients'][i].lower()
+    #     ingr_split = re.split(r'[.,]\s*(?![^()]*\))', ingr_i) # split on commas, ignore commas inside paren
+    #     products['ingredients_as_list'][i] = ingr_split
         
-        for i in ingr_split:
-            if i.strip() not in ingr_dict:
-                ingr_dict[i.strip()] = 1
-            else:
-                ingr_dict[i.strip()] += 1
+    #     for i in ingr_split:
+    #         if i.strip() not in ingr_dict:
+    #             ingr_dict[i.strip()] = 1
+    #         else:
+    #             ingr_dict[i.strip()] += 1
     
-    return(ingr_dict)
+    # return(ingr_dict)
+
+
+def get_parent(ingr):
+    return([i.split("(")[0].strip() for i in ingr])
+
+
+# Split ingredients column on commas, ignoring commas within parentheses
+def process_ingredients_to_list(ing_i):
+    # ing_i = prod_ing_i.str.lower()
+    return(re.split(r'[.,]\s*(?![^()]*\))', ing_i.lower()))
 
 
 # Makes ingredients dataframe with occurrence count in products dataframe
@@ -35,7 +47,8 @@ def make_ingredients_df(dict):
     return(ingr_df)
 
 
-# Splits an ingredients column into parent and child columns and returns
+
+# Returns a split and tokenized ingredients column into parent and child columns
 def split_parent_child(df_column):
     parent_child = df_column.str.split("(", expand = True)
     parent = parent_child[0].str.strip()
@@ -44,6 +57,7 @@ def split_parent_child(df_column):
     return(parent, child)
 
 
+# Make dummy columns and add to products dataframe.
 def make_dummies(products):
     # Make dummies for ingredients
     dummy_ingredients(products)
@@ -53,15 +67,15 @@ def make_dummies(products):
     
     return(products)
 
-# Create dummy variables for ingredients:
-# - Cocoa vs. chocolate?
+
+# Create dummy variables for extras/toppings:
 # - Peanuts
 # - Almonds
 # - Coffee
 # - Strawberries
 # - Raspberries
 # - Pecans
-# What others?
+# Others?
 def dummy_ingredients(products):
     dummies = ['peanuts', 'almonds', 'pecans', 'coffee', 'strawberries', 'raspberries', 'walnuts', 'rum']
 
@@ -71,25 +85,17 @@ def dummy_ingredients(products):
         products[col_name] = products['ingredients_as_list'].apply(lambda x: 1 if (d in x) else 0)
 
 
-def dummy_choc(products):
-    pass
-
-
-def contains_cocoa(products):
-    pass
-
-
-
 if __name__ == "__main__":
     wd = os.getcwd()
     products = pd.read_csv(wd + "/data/products.csv")
-
-    ingr_dict = split_ingredients(products)
-    ingredients_df = make_ingredients_df(ingr_dict)
+    
+    split_ingredients(products)
+    products = make_dummies(products)
+    # ingr_dict = split_ingredients(products)
+    # ingredients_df = make_ingredients_df(ingr_dict)
     # ingredients_df.to_csv(wd + "/data/ingredients.csv")
     # print(ingredients_df)
     
-    products = make_dummies(products)
     products.to_csv(wd + "/products_working.csv")
 
     
