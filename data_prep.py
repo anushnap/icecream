2ccf46e8c1746d52feed766ae0a578ddfe8dd53f
anushnap <anushnap@uw.edu>
2021-02-25 18:02:23 -0800

    fixed logic for finding parent ingredient list and updated ingredients file

--- data_prep.py
+++ data_prep.py
@@ -8,19 +8,7 @@ import re
 def split_ingredients(products):
     products['ingredients_as_list'] = products['ingredients'].apply(lambda x: process_ingredients_to_list(x))
     products['parent_ingredients_as_list'] = products['ingredients_as_list'].apply(lambda x: get_parent(x))
-
-    # for i in range(len(products)):
-    #     ingr_i = products['ingredients'][i].lower()
-    #     ingr_split = re.split(r'[.,]\s*(?![^()]*\))', ingr_i) # split on commas, ignore commas inside paren
-    #     products['ingredients_as_list'][i] = ingr_split
-        
-    #     for i in ingr_split:
-    #         if i.strip() not in ingr_dict:
-    #             ingr_dict[i.strip()] = 1
-    #         else:
-    #             ingr_dict[i.strip()] += 1
-    
-    # return(ingr_dict)
+    products['parent_ingredients_as_set'] = products['parent_ingredients_as_list'].apply(lambda x: set(x))
 
 
 def get_parent(ingr):
@@ -28,39 +16,52 @@ def get_parent(ingr):
 
 
 # Split ingredients column on commas, ignoring commas within parentheses
+# Ignore extra descriptors in ingredients list. 
+# Ex] 'Chocolate Ice Cream: Cream' in ingredients will return ['cream'] in list
 def process_ingredients_to_list(ing_i):
-    # ing_i = prod_ing_i.str.lower()
-    return(re.split(r'[.,]\s*(?![^()]*\))', ing_i.lower()))
+    initial_ingredients_list = re.split(r'[.,]\s*(?![^()]*\))', ing_i.lower())
+    removed_colons_list = []
+    for i in initial_ingredients_list:
+        if ":" in i:
+            removed_colons_list.append(i.split(":")[1].strip())
+        else:
+            removed_colons_list.append(i)
+
+    return(removed_colons_list)
 
 
 # Makes ingredients dataframe with occurrence count in products dataframe
 # Adds parent and child columns for ingredients where parent is ingredient prior to parentheses
 # Ex: LIQUID SUGAR (SUGAR, WATER): Parent is LIQUID SUGAR, child is SUGAR, WATER
-def make_ingredients_df(dict):
-    ingr_df = pd.DataFrame(ingr_dict.keys(), columns = ['Ingredient'])
-    ingr_df['count'] = ingr_dict.values()
-    ingr_df['percent_containing'] = ingr_df['count'] / len(products)
+def make_ingredients_df():
+    ingredients_dict = {}
 
-    # Split ingredients for parent/child ingredients
-    ingr_df['Parent'], ingr_df['Child'] = split_parent_child(ingr_df['Ingredient'])
+    products['parent_ingredients_as_set'].apply(lambda x: get_ingredients_keys(x, ingredients_dict))
+    for ingr in ingredients_dict.keys():
+        products['parent_ingredients_as_set'].apply(lambda x: increment_ingredients_dict(x, ingredients_dict, ingr))
+
+    ingr_df = pd.DataFrame(ingredients_dict.keys(), columns = ['Parent Ingredient'])
+    ingr_df['count'] = ingredients_dict.values()
+    ingr_df['percent_containing'] = ingr_df['count'] / len(products)
 
     return(ingr_df)
 
 
+def get_ingredients_keys(parent_list, ingredients_dict):
+    for i in parent_list:
+        if i not in ingredients_dict.keys():
+            ingredients_dict[i] = 1
 
-# Returns a split and tokenized ingredients column into parent and child columns
-def split_parent_child(df_column):
-    parent_child = df_column.str.split("(", expand = True)
-    parent = parent_child[0].str.strip()
-    child = parent_child[1].str.replace(")", "").str.strip()
 
-    return(parent, child)
+def increment_ingredients_dict(parent_list, ingredients_dict, ingredient_key):
+    if ingredient_key in parent_list:
+        ingredients_dict[ingredient_key] += 1
 
 
 # Make dummy columns and add to products dataframe.
 def make_dummies(products):
     # Make dummies for ingredients
-    dummy_ingredients(products)
+    dummy_topping(products)
 
     # Make dummies for brand
     products = pd.get_dummies(data = products, columns = ['brand'])
@@ -76,13 +77,13 @@ def make_dummies(products):
 # - Raspberries
 # - Pecans
 # Others?
-def dummy_ingredients(products):
-    dummies = ['peanuts', 'almonds', 'pecans', 'coffee', 'strawberries', 'raspberries', 'walnuts', 'rum']
+def dummy_topping(products):
+    dummies = ['peanuts', 'almonds', 'pecans', 'coffee', 'strawberries', 'raspberries', 'walnuts', 'rum', 'toffee']
 
     # Create new column for each desired dummy
     for d in dummies:
         col_name = "contains_" + d
-        products[col_name] = products['ingredients_as_list'].apply(lambda x: 1 if (d in x) else 0)
+        products[col_name] = products['parent_ingredients_as_set'].apply(lambda x: 1 if (d in x) else 0)
 
 
 if __name__ == "__main__":
@@ -91,11 +92,7 @@ if __name__ == "__main__":
     
     split_ingredients(products)
     products = make_dummies(products)
-    # ingr_dict = split_ingredients(products)
-    # ingredients_df = make_ingredients_df(ingr_dict)
-    # ingredients_df.to_csv(wd + "/data/ingredients.csv")
-    # print(ingredients_df)
-    
     products.to_csv(wd + "/products_working.csv")
-
+    ingredients_analysis = make_ingredients_df()
+    ingredients_analysis.to_csv(wd + "/ingredients.csv")
     
